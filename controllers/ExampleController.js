b5c288747d795b3ec3e39e25997494de37987323
barda <tomdar87@outlook.com>
2018-01-10 01:13:36 +0100

    commit du windows c'est de la merde

--- controllers/ExampleController.js
+++ controllers/ExampleController.js
@@ -1,4 +1,5 @@
 const exampleModels = require("../models/exampleModels");
+const Web3 = require('web3');
 
 module.exports.example = function(request, response) {
     response.title = "Accueil hein";
@@ -7,5 +8,12 @@ module.exports.example = function(request, response) {
         console.log(result);
         response.render("accueil", response);
     });*/
+    if (typeof web3 !== 'undefined') {
+        var web3 = new Web3(web3.currentProvider);
+    } else {
+        // set the provider you want from Web3.providers
+        var web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+    }
+    console.log(web3);
     response.render("accueil", response);
 };
