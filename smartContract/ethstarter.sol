3e5c22b9fbd8bb0b1216d663a98cc0dc69fa37f3
barda <tomdar87@outlook.com>
2018-01-13 22:23:11 +0100

    Ajout de commentaires au smart contract

--- smartContract/ethstarter.sol
+++ smartContract/ethstarter.sol
@@ -1,29 +1,32 @@
 pragma solidity ^0.4.16;
 contract Ethstarter {
+    //Stucture d'un contributeur
     struct Contributor{
         address addrContributor;
-        uint amountGiven;
+        uint amountGiven; //en Wei ( 1 ether = 1e18 wei)
     }
-
+    //Structure d'une campagne de financement
     struct Crowfunding{
         address addrContractor;
-        uint goal;
-        uint maxGoal;
-        uint amount;
+        uint goal; //en Wei ( 1 ether = 1e18 wei)
+        uint maxGoal; //en Wei ( 1 ether = 1e18 wei)
+        uint amount; //en Wei ( 1 ether = 1e18 wei)
         uint nbContributors;
         bool estEnCours;
         mapping(uint => Contributor) contributors;
     }
+    //Intances du contract
      mapping(uint => Crowfunding) public crowfundings;
      uint public nbCrowfundings;
-     //receiver.transfer(10);
 
+    //Ajout d'une campagne de financement
      function addCrowfunding(uint _id, address _addrContractor, uint _goal, uint _maxGoal) public{
         nbCrowfundings++;
         crowfundings[_id] = Crowfunding(_addrContractor, _goal, _maxGoal, 0, 0, true);
         crowfundings[_id].amount=0;
     }
 
+    //Ajout d'un contributeur à une campagne
     function addContributor(uint _idCrowfundig) public payable{
         Crowfunding storage c = crowfundings[_idCrowfundig];
         if(c.estEnCours){
@@ -32,6 +35,7 @@ contract Ethstarter {
         }
     }
 
+    //Cas où la campagne échoue, cette fonction envoie les ethers récoltés aux contributeurs.
     function sendToContributors(uint _id) public{
         Crowfunding storage c = crowfundings[_id];
         for(uint i=0;i<c.nbContributors;i++){
@@ -41,12 +45,14 @@ contract Ethstarter {
         }
     }
 
+    //Cas où la compagne réussie, cette fonction avoie les ethers récoltés à l'entrepreneur.
     function sendToContractor(uint _id) public{
         Crowfunding storage c = crowfundings[_id];
         c.addrContractor.transfer(c.amount);
         c.amount = 0;
     }
 
+    //Change le estEnCours d'une campagne.
     function setEstEnCours(uint _id, bool _estEnCours) public{
         Crowfunding storage c = crowfundings[_id];
         c.estEnCours = _estEnCours;
