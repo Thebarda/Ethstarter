4ef827e4b600af37ef682706a661a7f3a842376d
barda <tomdar87@outlook.com>
2018-01-15 15:17:24 +0100

    création du pattern graphique du site

--- controllers/ExampleController.js
+++ controllers/ExampleController.js
@@ -2,14 +2,14 @@ const exampleModels = require("../models/exampleModels");
 const ethstarterContract = require("../smartContract/ethstarterContract");
 
 module.exports.example = function(request, response) {
-    response.title = "Accueil hein";
+    response.title = "Ethstarter - accueil";
     /*exampleModels.getEditions(function (err, result) {
         if(err) throw err;
         console.log(result);
         response.render("accueil", response);
     });*/
-    console.log(""+ethstarterContract.addCrowfunding());
+    /*console.log(""+ethstarterContract.addCrowfunding());
     console.log(""+ethstarterContract.getBalance());
-    console.log("nbCrowfundings : "+ethstarterContract.getNbCrowfundings());
+    console.log("nbCrowfundings : "+ethstarterContract.getNbCrowfundings());*/
     response.render("accueil", response);
 };
---
+++ public/javascripts/main.js
@@ -0,0 +1,4 @@
+$(document).ready(function () {
+    //Sidenav always open
+    $(".button-collapse").sideNav();
+});
