b56e28bb245ee6db86ccc71efd6f0e4bb4fabaca
barda <tomdar87@outlook.com>
2018-01-15 19:09:07 +0100

    Ajout du début des modals de connexion et d'inscription

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     //Sidenav always open
     $(".button-collapse").sideNav();
-
+    $(".modal").modal();
     $(".btnActive").on("click", function(){
         $(".btnActive").each(function(){
            $(this).parent().removeClass("active");
