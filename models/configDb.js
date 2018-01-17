2fb355618c02d476b31f3d7e257dbf102fcf1636
barda <tomdar87@outlook.com>
2018-01-18 00:14:26 +0100

    Access denied for user \'ethstarter_admin\'@\'localhost\

--- models/configDb.js
+++ models/configDb.js
@@ -1,17 +1,27 @@
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize('database', 'username', 'password', {
+const sequelize = new Sequelize('ethstarterDB', 'ethstarter_admin', 'ethstarter_admin8587',{
     host: 'vps409515.ovh.net',
     dialect: 'mysql',
     pool: {
         max: 5,
         min: 0,
         acquire: 30000,
-        idle: 10000
+        idle: 60000
+    },
+    dialectOptions: {
+        socketPath: "/var/run/mysqld/mysqld.sock"
+    },
+    define: {
+        paranoid: true
     }
 });
 
-module.exports.testConnection = sequelize.authenticate().then(function(){
-    console.log('Connection has been established successfully.');
-}).catch(function(err){
-    console.error('Unable to connect to the database:', err);
-});
+exports.sequelize = sequelize;
+
+module.exports.testConnection = function() {
+    sequelize.authenticate().then(function(){
+        console.log('Connection has been established successfully.');
+    }).catch(function(err){
+        console.error('Unable to connect to the database:', err);
+    });
+};
