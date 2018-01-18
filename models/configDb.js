27a322d220ab0a2ea24c22430c08d78c89a29b08
barda <tomdar87@outlook.com>
2018-01-18 11:52:59 +0100

    On tej sequelize

--- models/configDb.js
+++ models/configDb.js
@@ -1,23 +1,15 @@
-const Sequelize = require('sequelize');
-const sequelize = new Sequelize('ethstarterDB', 'ethremotedb', 'leblancpresident2020',{
-    host: 'vps409515.ovh.net',
-    dialect: 'mysql',
-    pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 60000
-    },
-    define: {
-        timestamps: false
-    }
-});
-exports.sequelize = sequelize;
+var mysql = require('mysql');
 
-module.exports.testConnection = function() {
-    sequelize.authenticate().then(function(){
-        console.log('Connection has been established successfully.');
-    }).catch(function(err){
-        console.error('Unable to connect to the database:', err);
+var pool = mysql.createPool({
+    host: 'vps409515.ovh.net',
+    user: 'ethremotedb',
+    password: 'leblancpresident2020',
+    database: 'ethstarterDB',
+    port: '3306'
+});
+
+module.exports.getConnection = function(callback) {
+    pool.getConnection(function(err, connection) {
+        callback(err, connection);
     });
 };
