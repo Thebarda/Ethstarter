446838f03145d984d1c969b971904508d5074d03
barda <tomdar87@outlook.com>
2018-01-22 22:32:27 +0100

    test de test selenium

--- test/testSelenium.js
+++ test/testSelenium.js
@@ -1,11 +1,15 @@
 const {Builder, By} = require('selenium-webdriver');
+var assert = require('assert');
 describe("test selenium", function(){
     let driver = new Builder()
         .forBrowser('chrome')
         .build();
-    it('should faire de la merde', function(){
+    it('should faire de la merde', function(done){
        driver.get('http://localhost:1047/');
-       driver.findElement(By.tagName("h4"));
+       driver.findElement(By.tagName("h3")).getText().then(function(data){
+           assert.equal(data, "Salut");
+           done();
+       });
        driver.quit();
    });
 });
