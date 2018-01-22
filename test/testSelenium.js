30e7a7d8be604481a5993ec7d9bef6638d432716
barda <tomdar87@outlook.com>
2018-01-22 21:48:02 +0100

    ajout d'un test de selenium 2

---
+++ test/testSelenium.js
@@ -0,0 +1,11 @@
+const {Builder, By, until} = require('selenium-webdriver');
+
+let driver = new Builder()
+    .forBrowser('firefox')
+    .build();
+
+driver.get('http://www.google.com/ncr');
+driver.findElement(By.name('q')).sendKeys('webdriver');
+driver.findElement(By.name('btnK')).click();
+driver.wait(until.titleIs('webdriver - Google Search'), 1000);
+driver.quit();
