a15b8bdeb6e41b95a8f875d70dc306dfdfd713ed
barda <tomdar87@outlook.com>
2018-01-22 21:57:57 +0100

    debug travis 2

--- test/testSelenium.js
+++ test/testSelenium.js
@@ -1,11 +1,13 @@
 const {Builder, By, until} = require('selenium-webdriver');
-
-let driver = new Builder()
-    .forBrowser('chrome')
-    .build();
-
-driver.get('http://www.google.com/ncr');
-driver.findElement(By.name('q')).sendKeys('webdriver');
-driver.findElement(By.name('btnK')).click();
-driver.wait(until.titleIs('webdriver - Google Search'), 1000);
-driver.quit();
+describe("test selenium", function(){
+    let driver = new Builder()
+        .forBrowser('chrome')
+        .build();
+    it('should faire de la merde', function(){
+       driver.get('http://www.google.com/ncr');
+       driver.findElement(By.name('q')).sendKeys('webdriver');
+       driver.findElement(By.name('btnK')).click();
+       driver.wait(until.titleIs('webdriver - Google Search'), 1000);
+       driver.quit();
+   });
+});
