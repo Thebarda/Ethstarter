cbbc1adda4db702ac451350cd6b931bfb797e840
barda <tomdar87@outlook.com>
2018-01-25 15:17:52 +0100

    mide en place de l'architecture des tests

---
+++ test/models/campagnes.js
@@ -0,0 +1,9 @@
+var db = require('./configDb');
+
+module.exports.getAllCrowfundsThatFinishToday = function(callback){
+  db.getConnection(function(err, connection){
+    if(err) throw err;
+    connection.query("SELECT idCampagne, but, montantActuel FROM campagnes WHERE dateLimite LIKE DATE(NOW())", callback);
+    connection.release();
+  });
+};
---
+++ test/testConnectToDatatBase.js
@@ -0,0 +1,10 @@
+var db = require('../models/configDb');
+var assert = require('assert');
+describe("connect to database", function(){
+   it("should connect to database", function(done){
+     db.getConnection(function(err, connection){
+         assert.equal(err, null);
+         done();
+     });
+   });
+});
