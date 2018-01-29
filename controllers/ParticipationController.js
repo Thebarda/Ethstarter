bf5a09179d000edfe9fa86f4ab523ae0d0b50903
rotha <rotha.chheang021@gmail.com>
2018-01-29 15:13:58 +0100

    Avancée dans la participation des contributeurs

--- controllers/ParticipationController.js
+++ controllers/ParticipationController.js
@@ -1,7 +1,28 @@
 
-var modelCampagnes = require ('../models/campagnes.js');
+var modelCampagne = require ('../models/campagnes.js');
+var modelCampagne = require ('../models/participation.js');
 
 module.exports.validationParticipation = function(request, response){
     response.title = "Ethstarter - campagne";
-    
-}
+    var montant = request.body.montant;
+    var data = {idContributeur, montant};
+    modelCampagne.addParticipation(data, function(err, result){
+        if(err){
+            console.log(err);
+            return;
+        }
+    });
+    modelCampagne.addContributeursXCampagne(data, function(err, result){
+        if(err){
+            console.log(err);
+            return;
+        }
+    });
+    modelCampagne.updateMontant(idCampagne, montant, function(err, result){
+        if(err){
+            console.log(err);
+            return;
+        }
+    });
+    response.render("campagne", response);
+};
