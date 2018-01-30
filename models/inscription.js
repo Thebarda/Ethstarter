9dd916ababc44ba9233a2a3f715f17735bff3ae1
Himitris <antoine.gautier83@gmail.com>
2018-01-30 11:33:19 +0100

    Merge branch 'master' of https://github.com/Thebarda/Ethstarter into devAntoine
    # Conflicts:
    #	controllers/ConnexionController.js
    #	models/inscription.js
    #	package.json

--- models/inscription.js
+++ models/inscription.js
@@ -1,10 +1,12 @@
 var db = require('../configDB.js');
+var utils = require("../utils/utils");
+var sha256 = require('js-sha256').sha256;
 
 module.exports.valide = function(body, callback){
     //vérifie que le login n'est pas déjà prit
     db.getConnection(function(err, connexion){
         if(!err){
-            var sql = "SELECT login FROM utilisateur WHERE login='"+login+"';";
+            var sql = "SELECT login FROM utilisateur WHERE login='"+body.login+"';";
             connexion.query(sql, callback);
             connexion.release();
         }
@@ -12,11 +14,12 @@ module.exports.valide = function(body, callback){
 }
 
 module.exports.inscrire = function(body, callback){
-    db.getConnection(function(err, connexion){
-        if(err) throw err;
-
+    var mdp = sha256(body.password);
+    if (utils.isAddress(body.address) != "true") return false;
+    db.getConnection(function (err, connexion) {
+        if (err) throw err;
         var sql = "INSERT INTO utilisateur (nom, prenom, mail, login, password, addrPubliqueEth, type) VALUES(?,?,?,?,?,?,?);";
-        connexion.query(sql,[body.lastname,body.firstname,body.email,body.login,body.password,body.adress,'1'],function(err, results) {
+        connexion.query(sql, [body.lastname, body.firstname, body.email, body.login, mdp, body.address, '1'], function (err, results) {
             if (err) throw err;
         });
         connexion.release();
