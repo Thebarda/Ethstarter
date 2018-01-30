a2807b36323f9f85d08dfc65da5be7b8f6a27217
Romain Clavaud <rororom87@gmail.com>
2018-01-30 14:14:42 +0100

    Modif connexionController

--- controllers/ConnexionController.js
+++ controllers/ConnexionController.js
@@ -1,44 +1,49 @@
 var modelConnexion = require('../models/connexion.js');
 //var sha256 = require('js-sha256').sha256;
 
-
+// Méthode permettant d'afficher la page de connexion
 module.exports.connexion=function(request, response){
     response.title="Ethstarter - connexion";
     response.render('connexion', response);
 }
 
-
+// Méthode permettant de vérifier la correspondance du login et du mot de passe
 module.exports.validationConnexion = function(request, response){
     response.title="Validation connexion";
     if(request.session.isConnected){
         response.render('accueil', response);
     }else{
         var login = request.body.login;
-        var mdp = request.body.mdp;
-        modelConnexion.existeLogin(login, function(err, result){
+        var password = request.body.password;
+
+        // Log des champs saisis
+        console.log("++++  ID Connexion  ++++");
+        console.log("Login : " + login + "\nMDP : " + password);
+        console.log("++++++++++++++++++++++++");
+
+        // password = sha256(password);
+
+        // Vérification du login et du mot de passe dans la base de données
+        modelConnexion.verifConnexion(login, password, function(err, result){
             if(err){
                 console.log(err);
                 return;
             }
             if(result.length==0){
-                response.erreurLogin="Login incorrect";
+                console.log("Login ou password incorrect");
+                response.erreurConnexion="Login ou password incorrect !";
+                response.render("connexion", response);
+            }else{
+                console.log("Connecté !");
+                request.session.login=login;
+                request.session.isConnected=true;
+                response.render("accueil", response);
             }
-            // mdp = sha256(mdp);
-            modelConnexion.existePasswd(mdp, function(err, result){
-                if(result.length==0){
-                    response.erreurMdp="Mot de passe incorrect";
-                    response.render("connexion", response);
-                }else{
-                    request.session.login=login;
-                    request.session.isConnected="oui";
-                    response.render("accueil", response);
-                }
-            });
         });
     }
 }
 
-
+// Méthode permettant de se déconnecter
 module.exports.deconnexion=function(request, response){
     response.title="Deconnexion";
     request.session.destroy(function(err){
@@ -47,5 +52,6 @@ module.exports.deconnexion=function(request, response){
             return;
         }
         response.render('accueil', response);
+        console.log("Déconnecté !");
     });
 }
