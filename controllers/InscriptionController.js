c2ef9a1f5125166585f41dc21f8952f737dee824
Himitris <antoine.gautier83@gmail.com>
2018-01-30 16:00:01 +0100

    Merge branch 'master' of https://github.com/Thebarda/Ethstarter into devAntoine
    # Conflicts:
    #	models/inscription.js

--- controllers/InscriptionController.js
+++ controllers/InscriptionController.js
@@ -1,4 +1,5 @@
 var modelInscription = require('../models/inscription.js');
+var utils = require("../utils/utils");
 
 module.exports.inscription=function(request, response){
     response.title="Ethstarter - inscription contributeur";
@@ -8,15 +9,46 @@ module.exports.inscription=function(request, response){
 module.exports.validationInscriptionContributeur=function(request, response){
     response.title="Validation inscription";
     var body = request.body;
-    if(!modelInscription.existeLogin(body.login)){
-        alert('oui 1');
-    }
-    if(!modelInscription.existeMail(body.email)){
-        alert('oui 1');
-    }
-    if(!modelInscription.inscrire(body)){
-        alert('oui 2');
-    }
+    response.login=body.login;
+    response.email=body.email;
+    response.nom=body.lastname;
+    response.prenom=body.firstname;
+    response.address=body.address;
+    response.password=body.password;
+    modelInscription.existeLogin(body.login, function(err1, result1){
+        if(err1){
+            console.log(err);
+            return;
+        }
+        modelInscription.existeMail(body.email, function(err2, result2){
+            if(err2){
+                console.log(err);
+                return;
+            }
+            if(result1.length != 0){
+                console.log("Login déjà utilisé");
+                response.erreurInscription="Ce login est déjà utilisé";
+                response.render("inscription", response);
+            }else if(result2.length != 0){
+                console.log("E-mail déjà utilisé");
+                response.erreurInscription="Cet e-mail est déjà utilisé";
+                response.render("inscription", response);
+            }else{
+                if(utils.isAddress(body.address)){
+                    modelInscription.inscrireContributeur(body);
+                    response.inscrit="Vous êtes maintenant inscrit sur le site";
+                    console.log("Inscrit !");
+                    response.render("accueil", response);
+                }else{
+                    console.log("Adresse Ethereum invalide");
+                    response.erreurInscription="Adresse Ethereum invalide";
+                    response.render("inscription", response);
+                }
+            }
+        });
+    });
+}
 
-    response.render("accueil", response);
+module.exports.validationInscriptionContributeur=function(request, response){
+
 }
--- models/inscription.js
+++ models/inscription.js
@@ -1,48 +1,34 @@
 var db = require('../configDb.js');
-var utils = require("../utils/utils");
 var sha256 = require('js-sha256').sha256;
 
-module.exports.existeLogin = function(login){
+module.exports.existeLogin = function(login, callback){
     //vérifie que le login n'est pas déjà prit
     db.getConnection(function(err, connexion){
         if(!err){
             var sql = "SELECT login FROM utilisateur WHERE login='"+login+"';";
-            connexion.query(sql, function(err, results) {
-                if (err) throw err
-                if(typeof results[0] !== 'undefined') return false;
-            });
+            connexion.query(sql, callback);
             connexion.release();
         }
     });
-    return true;
 }
 
-module.exports.existeMail = function(email){
+module.exports.existeMail = function(email, callback){
     //vérifie que le mail n'est pas déjà prit
     db.getConnection(function(err, connexion){
         if(!err){
             var sql = "SELECT mail FROM utilisateur WHERE mail='"+email+"';";
-            connexion.query(sql, function(err, results) {
-                if (err) throw err
-                if(typeof results[0] !== 'undefined') return false;
-            });
+            connexion.query(sql, callback);
             connexion.release();
         }
     });
-    return true;
 }
 
-module.exports.inscrire = function(body){
+module.exports.inscrireContributeur = function(body){
     var mdp = sha256(body.password);
-    console.log(utils.isAddress(body.address));
-    if (!utils.isAddress(body.address)) return false;
-    console.log(utils.isAddress(body.address));
     db.getConnection(function (err, connexion) {
         if (err) throw err;
         var sql = "INSERT INTO utilisateur (nom, prenom, mail, login, password, addrPubliqueEth, type) VALUES(?,?,?,?,?,?,?);";
-        connexion.query(sql, [body.lastname, body.firstname, body.email, body.login, mdp, body.address, '1'], function (err, results) {
-            if (err) throw err;
-        });
+        connexion.query(sql, [body.lastname, body.firstname, body.email, body.login, mdp, body.address, '1']);
         connexion.release();
     });
 }
