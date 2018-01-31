665640ae96c845dc8a3fd1220794a2457b3872e3
Thomas C <tungsten.mail@gmail.com>
2018-01-31 14:14:16 +0100

    formatage

--- models/inscription.js
+++ models/inscription.js
@@ -2,33 +2,26 @@ var db = require('./configDb.js');
 var utils = require("../utils/utils");
 var sha256 = require('js-sha256').sha256;
 
-module.exports.valide = function(body, callback){
+module.exports.valide = function (body, callback) {
     //vérifie que le login n'est pas déjà prit
-    db.getConnection(function(err, connexion){
-        if(!err){
-            var sql = "SELECT login FROM utilisateur WHERE login='"+body.login+"';";
+    db.getConnection(function (err, connexion) {
+        if (!err) {
+            var sql = "SELECT login FROM utilisateur WHERE login='" + body.login + "';";
             connexion.query(sql, callback);
             connexion.release();
         }
     });
 }
 
-module.exports.inscrire = function(body, callback){
+module.exports.inscrire = function (body, callback) {
     var mdp = sha256(body.password);
+    if (utils.isAddress(body.address) != "true") return false;
     db.getConnection(function (err, connexion) {
         if (err) throw err;
         var sql = "INSERT INTO utilisateur (nom, prenom, mail, login, password, addrPubliqueEth, type) VALUES(?,?,?,?,?,?,?);";
-        connexion.query(sql, [body.lastname, body.firstname, body.email, body.login, mdp, body.address, body.type], callback);
+        connexion.query(sql, [body.lastname, body.firstname, body.email, body.login, mdp, body.address, '1'], function (err, results) {
+            if (err) throw err;
+        });
         connexion.release();
-    });
-}
-
-module.exports.inscrireEntrepreneur = function(idUtilisateur, nomEntreprise, pieceIdentide, callback){
-    db.getConnection(function(err, connexion){
-        if(!err){
-            var sql = "INSERT INTO entrepreneur(idUtilisateur, nomEntreprise, pieceIdentide) VALUES(?,?,?);";
-            connexion.query(sql, [idUtilisateur, nomEntreprise, pieceIdentide], callback);
-            connexion.release();
-        }
     });
 }
