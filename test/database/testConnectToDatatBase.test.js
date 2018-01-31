a9ddb09b855956fd11581ef76fda216bd714ae7c
barda <tomdar87@outlook.com>
2018-01-31 12:33:12 +0100

    debug travis 1

--- test/database/testConnectToDatatBase.test.js
+++ test/database/testConnectToDatatBase.test.js
@@ -1,10 +1,9 @@
 var db = require('../../models/configDb');
 var assert = require('assert');
 describe("connect to database", function(){
-   it("should connect to database", function(done){
-     db.getConnection(function(err, connection){
+   it("should connect to database", async function(){
+     await db.getConnection(function(err, connection){
          assert.equal(err, null);
-         done();
      });
    });
 });
