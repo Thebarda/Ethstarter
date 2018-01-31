4a9c70d292c1c5d98aa0206cc25c3c4e0c8c60d9
barda <tomdar87@outlook.com>
2018-01-31 10:49:13 +0100

    mise en place de l'inscription entrepreneur !

--- controllers/InscriptionController.js
+++ controllers/InscriptionController.js
@@ -1,5 +1,9 @@
 var modelInscription = require('../models/inscription.js');
 var utils = require("../utils/utils");
+var fs = require('fs');
+var formidable = require('formidable');
+var util = require('util');
+var fs = require('fs-extra');
 
 module.exports.inscription=function(request, response){
     response.title="Ethstarter - inscription";
@@ -8,6 +12,7 @@ module.exports.inscription=function(request, response){
 
 module.exports.validationInscriptionContributeur=function(request, response){
     var body = request.body;
+    body.type = 1;
     console.log(body);
     if(utils.isAddress(body.address)) {
         response.title="Validation inscription";
@@ -16,15 +21,51 @@ module.exports.validationInscriptionContributeur=function(request, response){
                 console.log(err);
                 return;
             }
-            if (result.length == 0) {
+            if (result.length != 0) {
                 response.erreurLogin = "Login incorrect";
+                response.render("inscription", response);
+            }else{
+                modelInscription.inscrire(body,function(err, result){
+                    if(err) throw err;
+                    response.render("connexion", response);
+                });
             }
-            modelInscription.inscrire(body);
         });
-        response.render("connexion", response);
     }else{
         response.title="Ethstarter - inscription";
         response.erreurAddr = "Adresse publique incorrecte";
         response.render("inscription", response);
     }
+}
+
+module.exports.validationInscriptionEntrepreneur=function(request, response){
+    var form = new formidable.IncomingForm();
+    form.parse(request, function (err, fields, files) {
+        var body = util.inspect({fields: fields});
+        var lastname = fields.lastname;
+        var firstname = fields.firstname;
+        var login = fields.login;
+        var password = fields.password;
+        var nomEntreprise = fields.nomEntreprise;
+        var address = fields.address;
+        fields.type=2;
+        var path = "./public/PIpics/"+lastname+firstname+nomEntreprise+files.image.name;
+        fs.copy(files.image.path, path, function (err) {
+            if (err) {
+                console.error(err);
+                response.render('inscription', response);
+            } else {
+                console.log("success!");
+                modelInscription.valide(fields, function(err, result){
+                    if(err) throw err;
+                    modelInscription.inscrire(fields, function(err, result){
+                        modelInscription.inscrireEntrepreneur(result.insertId, nomEntreprise, path, function(err, result){
+                           if(err) throw err;
+                            response.render("connexion", response);
+                        });
+                    });
+                });
+            }
+        });
+    });
 }
--- models/inscription.js
+++ models/inscription.js
@@ -15,13 +15,20 @@ module.exports.valide = function(body, callback){
 
 module.exports.inscrire = function(body, callback){
     var mdp = sha256(body.password);
-    if (utils.isAddress(body.address) != "true") return false;
     db.getConnection(function (err, connexion) {
         if (err) throw err;
         var sql = "INSERT INTO utilisateur (nom, prenom, mail, login, password, addrPubliqueEth, type) VALUES(?,?,?,?,?,?,?);";
-        connexion.query(sql, [body.lastname, body.firstname, body.email, body.login, mdp, body.address, '1'], function (err, results) {
-            if (err) throw err;
-        });
+        connexion.query(sql, [body.lastname, body.firstname, body.email, body.login, mdp, body.address, body.type], callback);
         connexion.release();
+    });
+}
+
+module.exports.inscrireEntrepreneur = function(idUtilisateur, nomEntreprise, pieceIdentide, callback){
+    db.getConnection(function(err, connexion){
+        if(!err){
+            var sql = "INSERT INTO entrepreneur(idUtilisateur, nomEntreprise, pieceIdentide) VALUES(?,?,?);";
+            connexion.query(sql, [idUtilisateur, nomEntreprise, pieceIdentide], callback);
+            connexion.release();
+        }
     });
 }
