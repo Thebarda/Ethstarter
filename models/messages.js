a7efb89e3cbb1a80ddfe014da0f948c4c7626a88
tngstn <tngstn>
2018-03-26 01:43:06 +0200

    dm crud

--- models/messages.js
+++ models/messages.js
@@ -1,11 +1,22 @@
 var db = require('./configDb');
 
 module.exports.getAll = async (userID) => {
-    var query = "SELECT * FROM `dm` WHERE idUtilisateur = " + userID;
+    var query = "SELECT * FROM dm WHERE idUtilisateur = " + userID;
     return db.asq(query);
 }
 
 module.exports.get = async (messID) => {
-    var query = "SELECT * FROM `dm` WHERE idDM = " + messID;
+    var query = "SELECT * FROM dm WHERE idDM = " + messID;
     return db.asq(query);   
+}
+
+module.exports.write = async (userID, title, message, timestamp) => {
+    var query = "INSERT INTO dm VALUES (default," + userID + 
+                "," + title + "," + message + "," + timestamp + ")";
+    db.asq(query);
+}
+
+module.exports.delete = async (messID) => {
+    var query = "DELETE FROM dm WHERE idDM = " + messID;
+    db.asq(query);
 }
