c5ab3c1d0b0c974a035fedb1d99e87ead7065c52
Himitris <antoine.gautier83@gmail.com>
2018-03-26 15:27:01 +0200

    Merge branch 'master' of https://github.com/Thebarda/Ethstarter into devAntoine
    # Conflicts:
    #	app.js
    #	models/campagnes.js
    #	package.json
    #	views/afficherMesCampagnes.handlebars

--- public/javascripts/graphCampagne.js
+++ public/javascripts/graphCampagne.js
@@ -1,5 +1,36 @@
-$(document).ready(function () {
+var campagnesModel = require("../models/campagnes");
+
+$(document).ready(function(){
+    $.get(campagnesModel.getDonateurs(), function(response) {
+        if (!response.length) {
+            console.log('une erreur est survenue');
+            return false;
+        }
+        getChart(response);
+    }, 'json');
 
 
-
+    // Bar chart
+    new Chart(document.getElementById("bar-chart"), {
+        type: 'bar',
+        data: {
+            labels: ["Africa", "Asia", "Europe", "Latin America", "North America"],
+            datasets: [
+                {
+                    label: "Population (millions)",
+                    backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850"],
+                    data: [2478,5267,734,784,433]
+                }
+            ]
+        },
+        options: {
+            legend: { display: false },
+            title: {
+                display: true,
+                text: 'Predicted world population (millions) in 2050'
+            }
+        }
+    });
 });
+
+
