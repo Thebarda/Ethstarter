6c21e5ba4ea9e6014c7b50ea5445c02dd606d297
Himitris <antoine.gautier83@gmail.com>
2018-03-28 11:26:08 +0200

    Merge branch 'master' of https://github.com/Thebarda/Ethstarter into devAntoine
    # Conflicts:
    #	app.js
    #	models/campagnes.js
    #	package.json
    #	views/afficherMesCampagnes.handlebars

--- public/javascripts/graphCampagne.js
+++ public/javascripts/graphCampagne.js
@@ -1,36 +1,47 @@
-var campagnesModel = require("../models/campagnes");
-
 $(document).ready(function(){
-    $.get(campagnesModel.getDonateurs(), function(response) {
+    function getChart(datas) {
+        console.log(datas);
+        json_encode(datas);
+        console.log(datas);
+        // Bar chart
+        new Chart(document.getElementById("bar-chart"), {
+            type: 'bar',
+            data: {
+                labels: ["Africa", "Asia", "Europe", "Latin America", "North America"],
+                datasets: [
+                    {
+                        label: "Population (millions)",
+                        backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850"],
+                        data: [2478,5267,734,784,433]
+                    }
+                ]
+            },
+            options: {
+                legend: { display: false },
+                title: {
+                    display: true,
+                    text: 'Predicted world population (millions) in 2050'
+                }
+            }
+        });
+    }
+
+    $.ajax({
+        url:"/campagnes",
+        method:"post",
+        data:{montant:$("#montantJS").val()}
+    }).done(function(json){
+        console.log("oui" + response);
+        getChart(data);
+    });
+    /*$.get(campagnesModel.getDonateurs(), function(response) {
         if (!response.length) {
             console.log('une erreur est survenue');
             return false;
         }
+        console.log("oui" + response);
         getChart(response);
-    }, 'json');
-
-
-    // Bar chart
-    new Chart(document.getElementById("bar-chart"), {
-        type: 'bar',
-        data: {
-            labels: ["Africa", "Asia", "Europe", "Latin America", "North America"],
-            datasets: [
-                {
-                    label: "Population (millions)",
-                    backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850"],
-                    data: [2478,5267,734,784,433]
-                }
-            ]
-        },
-        options: {
-            legend: { display: false },
-            title: {
-                display: true,
-                text: 'Predicted world population (millions) in 2050'
-            }
-        }
-    });
+    }, 'json');*/
 });
 
 
