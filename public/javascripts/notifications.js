cf144f2b05fae9655d7f4bd9812fb23aa0fb89ab
tngstn <tngstn>
2018-04-04 16:17:46 +0200

    sidebar fix

--- public/javascripts/notifications.js
+++ public/javascripts/notifications.js
@@ -1,13 +1,19 @@
-function searchFunc(e){
+$(document).ready(function(){
+    //Tab management
+    $('.tabs').tabs();
+
+});
+
+function searchFunc(e) {
     $(document).ready(() => {
         console.log('salut');
         var search = e.target.value;
-        $(".textNotifs").each(function(){
-           if($(this).children().text().includes(search)) {
-               $(this).show(200);
-           } else {
-               $(this).hide(200);
-           }
+        $(".textNotifs").each(function () {
+            if ($(this).children().text().includes(search)) {
+                $(this).show(200);
+            } else {
+                $(this).hide(200);
+            }
         });
     });
 }
