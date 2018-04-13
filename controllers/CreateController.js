8d67188aab212200e6a3833cb709f20f569e722b
tngstn <tngstn>
2018-04-13 03:58:39 +0200

    img upload backend - untested

--- controllers/CreateController.js
+++ controllers/CreateController.js
@@ -14,21 +14,34 @@ module.exports.validationCampagne = function (request, response) {
     response.title = "Ethstarter - Création campagne";
     var body = request.body;
     body.idEntrepreneur = request.session.idCompte;
-    //body.image = "DUMMY PATH";
     body.validated = 0;
+    
+    //img handling ---- todo : file upload constrains (see express fileup doc)
+    if (!request.files) console.log("file was not upladed - front end");
+    var coverimg = request.files.coverimg;
+    if (coverimg.mimetype != "image/jpeg0") console.log("FUCK OFF"); //check for correct file type 
 
-    createModels.insertCampaign(body, function (err, result) {
-        if (err) {
-            console.log(err);
-            return;
-        }
-        else {
-            response.response = result.insertId;
-            ethstarterContract.addCrowfunding(result.insertId, request.session.addrPubliqueEth, parseInt(body.but), parseInt(body.montantMax));
-            var texte = "Votre campagne "+body.nomCampagne+" est en attente de validation";
-            notifModel.addNotification(request.session.idCompte, texte, (err, result2) => {
-                response.render("emptyView", response);
-            });
-        }
+    var UUIDname = "mock";
+    var extRegex = /\.[0-9a-z]+$/i; 
+    var ext = coverimg.name.match(extRegex);
+    body.image = UUIDname + ext; //HAS TO CHANGE, USING UUID
+
+    coverimg.mv('../public/images/uploads/' + UUIDname + ext , function(err) { //USE A PROMISE
+        if (err) return err;
+
+        createModels.insertCampaign(body, function (err, result) {
+            if (err) {
+                console.log(err);
+                return;
+            }
+            else {
+                response.response = result.insertId;
+                ethstarterContract.addCrowfunding(result.insertId, request.session.addrPubliqueEth, parseInt(body.but), parseInt(body.montantMax));
+                var texte = "Votre campagne "+body.nomCampagne+" est en attente de validation";
+                notifModel.addNotification(request.session.idCompte, texte, (err, result2) => {
+                    response.render("emptyView", response);
+                });
+            }
+        });
     });
 }
