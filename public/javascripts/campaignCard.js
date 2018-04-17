0032318a0be9d09c1cd73e23aeacdb6becfb3b8f
tngstn <tngstn>
2018-04-18 01:51:51 +0200

    card reveal hover

---
+++ public/javascripts/campaignCard.js
@@ -0,0 +1,42 @@
+$(function() {
+/*     $('.card').hover(
+        function() {
+            $(this).find('> .card-image > img.activator').click();
+        }, function() {
+            $(this).find('> .card-reveal > .card-title').click();
+        }
+    );  */
+
+    $(".card").mouseenter(function(e){
+        if ($(this).find('> .card-reveal').length) {
+            if ($(e.target).is($('.card .activator')) || $(e.target).is($('.card .activator i')) ) {
+                // Make Reveal animate up
+                $(this).find('.card-reveal').css({ display: 'block'}).velocity("stop", false).velocity(
+                    {translateY: '-100%'}, 
+                    {
+                        duration: 300, 
+                        queue: false, 
+                        easing: 'easeInOutQuad'
+                    });
+            }
+        }
+    
+        $('.card-reveal').closest('.card').css('overflow', 'hidden');
+    
+    });
+    
+    $(".card").mouseleave(function(){
+        $(this).find('.card-reveal').velocity(
+            {translateY: 0}, 
+            {
+                duration: 225,
+                queue: false,
+                easing: 'easeInOutQuad',
+                complete: function() {
+                    $(this).css({ display: 'none'});
+                }
+            });
+    });
+
+});
+
