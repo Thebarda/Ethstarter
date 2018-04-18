d9e8086946c4f4357cf3a3186584595c4f6760ee
tngstn <tngstn>
2018-04-18 02:01:05 +0200

    create ctrler refac + placeholder

--- controllers/CreateController.js
+++ controllers/CreateController.js
@@ -15,19 +15,23 @@ module.exports.validationCampagne = function (request, response) {
     var body = request.body;
     body.idEntrepreneur = request.session.idCompte;
     body.validated = 0;
+    body.image = "placeholder2.jpg"
     
     //img handling ---- todo : file upload constrains (see express fileup doc)
     var coverimg = request.files.coverimg;
     var accepted = true;
-    if (!coverimg) console.log("file was not upladed - front end");
-    if (coverimg.mimetype != "image/jpeg" && 
+
+    if (coverimg){
+        var extRegex = /\.[0-9a-z]+$/i; 
+        var ext = coverimg.name.match(extRegex);
+        var filename = utils.genUUID() + ext[0];
+        body.image = filename;
+
+        if (coverimg.mimetype != "image/jpeg" && 
         coverimg.mimetype != "image/bmp" &&
         coverimg.mimetype != "image/png") accepted = false; //check for correct file type 
-
-    var extRegex = /\.[0-9a-z]+$/i; 
-    var ext = coverimg.name.match(extRegex);
-    var filename = utils.genUUID() + ext[0];
-    body.image = filename;
+    }
+    else { console.log("file was not upladed - front end") }
 
     createModels.insertCampaign(body, function (err, result) {
         if (err) {
@@ -40,7 +44,7 @@ module.exports.validationCampagne = function (request, response) {
             var texte = "Votre campagne "+body.nomCampagne+" est en attente de validation";
             notifModel.addNotification(request.session.idCompte, texte, (err, result2) => {
             
-                if (accepted) {
+                if (accepted && coverimg) {
                     coverimg.mv("public/images/uploads/" + filename, function(err) {
                         if (err) return err;
                         console.log("UPLOADED");
